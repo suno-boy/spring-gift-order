853653d5873d5ad743f755073916a65c94f29398
suno-boy <tnsgh3691@gmail.com>
2024-07-27 01:25:21 +0900

    feat: KaKaoUserRepository 구현

---
+++ src/main/java/gift/Repository/KakaoUserRepository.java
@@ -0,0 +1,8 @@
+package gift.Repository;
+
+import gift.Entity.KakaoUserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KakaoUserRepository extends JpaRepository<KakaoUserEntity, Long> {
+    KakaoUserEntity findByKakaoId(String kakaoId);
+}
